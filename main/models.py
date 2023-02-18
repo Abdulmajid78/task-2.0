a50324e09e39cbec318abe131ffb67613c3f0e20
Abraham <jayson060702@gmail.com>
2023-02-18 16:39:55 +0500

    created main app

---
+++ main/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class ContactMessageModel(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'comment'
+        verbose_name_plural = 'comments'
+
+
+class BannerModel(models.Model):
+    collection = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='banners/')
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'banner'
+        verbose_name_plural = 'banners'
---
+++ main/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, reverse
+
+from django.views.generic import TemplateView, CreateView
+from .models import BannerModel
+from .forms import ContactForm
+
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['contact_form'] = ContactForm()
+        return context
+
+
+class ContactCreateView(CreateView):
+    form_class = ContactForm
+
+    def get_success_url(self):
+        return reverse('main:contact')
+
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['banners'] = BannerModel.objects.filter(status=True)
+        return context
